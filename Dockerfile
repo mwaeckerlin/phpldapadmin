61539836244ab1644b721dd7102f56c47f480969
Marc Wäckerlin <marc@waeckerlin.org>
2016-03-07 19:37:48 +0100

    works

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,12 @@ FROM ubuntu
 MAINTAINER mwaeckerlin
 ENV TERM "xterm"
 
-ENV PASSWORD ""
+ENV USER ""
 
 EXPOSE 80
-EXPOSE 443
 
-ADD start.sh /start.sh
 RUN apt-get update
 RUN apt-get install -y phpldapadmin
 
+ADD start.sh /start.sh
 CMD /start.sh
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#/bin/bash -ex
+#!/bin/bash -e
 
 if test -z "${LDAP_ENV_DOMAIN}"; then
     echo "**** ERROR: please link to an mwaeckerlin/openldap container" 1>&2
@@ -9,8 +9,8 @@ fi
 DOMAIN="dc=${LDAP_ENV_DOMAIN//./,dc=}"
 
 sed -e 's|\(\$servers->setValue('"'"'server'"'"','"'"'host'"'"','"'"'\).*\('"'"');\)|\1ldap\2|' \
-    -e 's|\(\$servers->setValue('"'"'server'"'"','"'"'base'"'"',array('"'"'\).*\('"'"'));\)|\1'"${DOMAIN}"'\2|' \
-    -e 's|\(\$servers->setValue('"'"'login'"'"','"'"'bind_id'"'"','"'"'\).*\('"'"');\)|\1'"cn=admin,${DOMAIN}"'\2|' \
+    -e 's|\(\$servers->setValue('"'"'server'"'"','"'"'base'"'"',array('"'"'\).*\('"'"'));\)|\1'"cn=config','${DOMAIN}"'\2|' \
+    -e 's|\(\$servers->setValue('"'"'login'"'"','"'"'bind_id'"'"','"'"'\).*\('"'"');\)|\1'"${USER:-cn=admin,${DOMAIN}}"'\2|' \
     -e 's|.*\(\$config->custom->appearance\['"'"'hide_template_warning'"'"'\] = \).*\(;\)|\1true\2|' \
     -i /etc/phpldapadmin/config.php
 
